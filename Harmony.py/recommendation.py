134657670a63a9e876245ae662ea33756420df51
Yoitsjae <ncubejason896@gmail.com>
2024-02-08 18:14:23 +0200

    harmonyUpdate

--- Harmony.py/recommendation.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def generate_recommendations(genre_artists, genre, artists):
-    recommendations = []
-    for artist in artists:
-        similar_artists = [a for a in genre_artists[genre] if a != artist]
-        recommendations.extend(random.sample(similar_artists, min(2, len(similar_artists))))
-    return recommendations
