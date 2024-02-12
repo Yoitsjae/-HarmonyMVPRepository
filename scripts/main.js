26c8d3b10dc1609e5d3c4fa2e6826dc9ebe72e43
Yoitsjae <ncubejason896@gmail.com>
2024-02-12 15:20:18 +0200

    readme

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,25 @@
-// scripts/main.js
+// Get the modal element
+var modal = document.getElementById("myModal");
 
-// Smooth scrolling functionality for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
-  });
-});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
